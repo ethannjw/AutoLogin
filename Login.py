524ba0e15d2a13d38b6dbff36760af5b0c97340f
ethan <ethannjw@gmail.com>
2020-03-01 15:47:53 +0800

    second commit. no chromedriver.exe

--- Login.py
+++ Login.py
@@ -1,15 +1,24 @@
+"""
+Code currently works in windows
+Requires the following:
+python3
+pip install selenium
+chromedriver in PATH
+
+
+"""
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
-
+from selenium.webdriver.chrome.options import Options
 
 class Login:
 
     def __init__(self, url='http://quotes.toscrape.com/login', username="uname", password="password1"):
-        options = webdriver.ChromeOptions()
-        options.add_argument("--start-maximized")
-        options.add_argument("--disable-infobar")
-        self.driver = webdriver.Chrome('chromedriver.exe', chrome_options=options)
+        options = Options()
+        options.add_argument("start-maximized")
+        options.add_argument("disable-notifications")
+        self.driver = webdriver.Chrome(chrome_options=options)
         print("Loading driver browser")
         self.driver.get(url)
         print("Opened login URL")
